d4d02c3f0d5f97921853e1c1aacdcd5a84a8cf52
ouob <kyjin320@sookmyung.ac.kr>
2023-11-05 12:32:44 +0900

    Feat: [1주차] 빌드, 테스트 코드 수정

--- 김유진/freelec-springboot2-webservice/src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
+++ 김유진/freelec-springboot2-webservice/src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -1,10 +1,7 @@
 package com.jojoldu.book.springboot.web.dto;
 
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-// import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class HelloResponseDtoTest {
 
