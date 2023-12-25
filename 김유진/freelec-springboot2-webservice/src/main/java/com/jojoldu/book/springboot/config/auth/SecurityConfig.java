296102c8169ba28d38bafff37dc1eff110801fc4
ouob <kyjin320@sookmyung.ac.kr>
2023-12-25 23:48:05 +0900

    Feat: [5주차] 지난 실습 오류 수정

--- 김유진/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
+++ 김유진/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -12,6 +12,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomOAuth2UserService customOAuth2UserService;
 
+    // 오류 수정
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
